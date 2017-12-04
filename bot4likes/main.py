2761f182652c9609ae1eda26504760143a1b572d
dvec <dvecend@gmail.com>
2017-12-04 23:15:37 +0400

    Bug fixes

--- bot4likes/main.py
+++ bot4likes/main.py
@@ -1,5 +1,5 @@
 from bot4likes.config import api_group_token, api_user_login, api_user_password
-from bot4likes.longpoll import LongPoll
+from bot4likes.handler import Handler
 
 if __name__ == '__main__':
-    LongPoll(group_token=api_group_token, user_login=api_user_login, user_password=api_user_password).handle()
+    Handler(group_token=api_group_token, user_login=api_user_login, user_password=api_user_password).handle()
