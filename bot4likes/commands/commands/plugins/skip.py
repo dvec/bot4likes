1ce36d628255c515a39697c32f74ca2bdb78bf3f
dvec <dvecend@gmail.com>
2017-12-07 00:41:29 +0400

    Fixed skip command

--- bot4likes/commands/commands/plugins/skip.py
+++ bot4likes/commands/commands/plugins/skip.py
@@ -3,12 +3,15 @@ from bot4likes.domain.database import database
 
 
 class SkipCommand(Command):
-    names = ['п', 'пропустить']
+    names = ['пр', 'пропустить']
     description = 'пропустить данное задание'
 
     @staticmethod
     def process(user, parsed, api, attachments):
-        with database.transaction():
-            user.tasks_done.append(user.current_task)
-            user.current_task = None
-            user.save()
+        if user.current_task:
+            with database.transaction():
+                user.tasks_done.append(user.current_task)
+                user.current_task = None
+                user.save()
+        else:
+            return 'У вас еще нет таска'
