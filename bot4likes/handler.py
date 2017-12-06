ca9b46bfe4c5b632e79df00805db54221ea0bf22
dvec <dvecend@gmail.com>
2017-12-06 22:18:21 +0400

    Code refactoring

--- bot4likes/handler.py
+++ bot4likes/handler.py
@@ -21,14 +21,25 @@ class Handler:
         self.user_sess.auth()
         self.user_api = self.user_sess.get_api()
 
-        self.long_poll = self.__get__long_poll()
+        self.long_poll = self._get__long_poll()
         self.command_manager = CommandManager()
 
-    def __handle_message(self, event):
+    def _send_message(self, user_id, text):
+        while 1:
+            try:
+                self.group_api.messages.send(user_id=user_id, message=text)
+            except Exception as e:
+                logging.exception(e)
+                sleep(10)
+            else:
+                logging.info("Send text: {}".format(text.replace('\n', ' ')))
+                break
+
+    def _handle_message(self, event):
         logging.info("Performing message: {}".format(event.text))
 
         try:
-            result = self.command_manager.process(self.__get_current_user(event.user_id), self.user_api, event)
+            result = self.command_manager.process(self._get_current_user(event.user_id), self.user_api, event)
         except Exception as e:
             logging.exception(e)
             result = 'Произошла внутренняя ошибка. Повторите попытку позже'
@@ -36,17 +47,9 @@ class Handler:
         if not result:
             result = 'Готово'
 
-        while 1:
-            try:
-                self.group_api.messages.send(user_id=event.user_id, message=result)
-            except Exception as e:
-                logging.exception(e)
-                sleep(10)
-            else:
-                logging.info("Send answer: {}".format(result.replace('\n', ' ')))
-                break
+        self._send_message(event.user_id, result)
 
-    def __get_current_user(self, user_id):
+    def _get_current_user(self, user_id):
         try:
             return User.get(user_id=user_id)
         except User.DoesNotExist:
@@ -61,7 +64,7 @@ class Handler:
                               reward=Task().select(fn.avg(Task.reward) + 1).where(Task.type == TaskType.LIKE_TYPE))
                 return user
 
-    def __get__long_poll(self):
+    def _get__long_poll(self):
         while 1:
             try:
                 return VkLongPoll(self.group_sess)
@@ -69,7 +72,7 @@ class Handler:
                 logging.exception(e)
                 sleep(10)
 
-    def __listen_long_poll(self):
+    def _listen_long_poll(self):
         while True:
             try:
                 events = self.long_poll.listen()
@@ -80,6 +83,6 @@ class Handler:
                 self.long_poll.update_longpoll_server()
 
     def handle(self):
-        for event in self.__listen_long_poll():
+        for event in self._listen_long_poll():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                threading.Thread(target=self.__handle_message, args=[event]).start()
+                threading.Thread(target=self._handle_message, args=[event]).start()
