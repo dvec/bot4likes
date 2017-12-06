4b6e7955bad4bdc89de4eafd6b49c7cb2ffb711d
dvec <dvecend@gmail.com>
2017-12-06 20:59:45 +0400

    Bug fixes

--- bot4likes/commands/commands/plugins/tasks.py
+++ bot4likes/commands/commands/plugins/tasks.py
@@ -9,7 +9,7 @@ class TasksCommands(Command):
 
     @staticmethod
     def process(user, parsed, api, attachments):
-        tasks = Task().select().where(Task.customer_id == user.user_id)
+        tasks = Task().select().where(Task.customer_id == user.id)
         tasks = ['{}. {} {}{}{}_{} за {}'.format(t.id, STR_TYPES[t.type], vk_url, t.content_type,
                                                  t.owner_id, t.item_id, t.reward) for t in tasks]
         return '\n'.join(tasks) if len(tasks) != 0 else 'У вас нет тасков'
